a7cc6c6a698b59ff07e95d7d4ce12584e23a1e01
justinlesnicki3 <justinlesnicki5563@aol.com>
2026-03-12 22:02:38 -0500

    worked on login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,9 +1,12 @@
-import { View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
+import { View, Text, Image, StyleSheet, TouchableOpacity, TextInput} from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {themeColors} from '../theme'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
 import { useNavigation } from '@react-navigation/native'
+import loginIMG from '../assets/images/logIn.png'
+import GoogleIMG from '../assets/images/Google_logo.png'
+import AppleIMG from '../assets/images/apple_logo.png'
 
 export default function LoginScreen() {
     const navigation = useNavigation();
@@ -17,20 +20,64 @@ export default function LoginScreen() {
                         <ArrowLeftIcon size="30" color="black"></ArrowLeftIcon>
                     </TouchableOpacity>
                 </View>
-                <View>
-                    
+                <View style={styles.imagePlacement}>
+                    <Image source={loginIMG} style={styles.loginImage}/>
                 </View>
             </SafeAreaView>
+            <View style={styles.textBox}>
+                <View style={styles.emailBox}>
+                    <Text style={styles.emailText}>Email Address</Text>
+                    <TextInput
+                        style={styles.textInput}
+                        placeholder='Enter Email'
+                    />
+                    <Text style={styles.emailText}>Password</Text>
+                    <TextInput
+                        style={styles.textInput}
+                        secureTextEntry={true}
+                        placeholder='Enter Password'
+                    />
+                    <TouchableOpacity style={styles.forgotButton}>
+                        <Text>Forgot Password?</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.loginButton}>
+                        <Text style={styles.loginText}>Login</Text>
+                    </TouchableOpacity>
+                </View>
+                <Text style={styles.orOption}>
+                    Or
+                </Text>
+                <View style={styles.optionalSocials}>
+                    <TouchableOpacity style={styles.iconButtons}>
+                        <Image source={GoogleIMG} style={styles.googleImage} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.iconButtons}>
+                        <Image source={AppleIMG} style={styles.googleImage} />
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create ({
 
-    container: { flex: 1, backgroundColor: themeColors.bg },
+    container: { flex: 1, backgroundColor: '#D8B4FE' },
     areaView: {flex: 1},
     arrowIcon: {backgroundColor: 'white', padding: 8, borderTopRightRadius: 16, borderBottomLeftRadius: 16, marginLeft: 16, width: 45},
-
+    loginImage: {width: 300, height: 300},
+    imagePlacement: {flexDirection: 'row', justifyContent: 'center'},
+    textBox: {flex: 1, backgroundColor: 'white', paddingHorizontal: 8, paddingTop: 8, borderTopLeftRadius: 50, borderTopRightRadius: 50},
+    emailBox: {marginBottom: 8, marginTop: 10},
+    emailText: {color: '#374151', marginLeft: 25, paddingTop: 10, paddingBottom: 5},
+    textInput: {padding: 16, backgroundColor: '#F3F4F6', color: '#374151', borderRadius: 16, marginHorizontal: 30, paddingTop: 15, marginBottom: 10},
+    forgotButton: {alignItems: 'flex-end', color: '#374151', marginBottom: 5, marginRight: 35},
+    loginButton: {paddingVertical: 12, backgroundColor: '#FBBF24', borderRadius: 12, marginHorizontal: 45, marginTop: 40},
+    loginText: {fontWeight: 'bold', textAlign: 'center', color: '#374151'},
+    orOption: {fontSize: 20, color: '#374151', fontWeight: 'bold', textAlign: 'center', paddingVertical: 20},
+    googleImage: {width: 35, height: 35},
+    optionalSocials: {flexDirection: 'row', justifyContent: 'center', gap: 38},
+    iconButtons: {padding: 8, backgroundColor: '#F3F4F6', borderRadius: 16}
 
 
 })
